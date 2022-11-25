0658fc83a233d52a3863ae0f1baa9882be1ed8c0
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-11-25 12:14:54 +0100

    🔧  Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["src/**/*.test.ts"],
+      env: {
+        jest: true,
+      },
+      extends: ["plugin:jest/recommended"],
+      plugins: ["jest"],
+    },
+  ],
+};
