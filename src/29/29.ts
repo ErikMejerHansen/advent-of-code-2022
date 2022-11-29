ad39d20c578b34a54973b92a8bcb5863e721a2e4
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-11-29 12:14:37 +0100

    ➕ add ts-node to fix the 'today:*' scripts

---
+++ src/29/29.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs'
+
+const data = fs.readFileSync('./src/29/data/data.txt').toString()
+
---
+++ src/29/__tests__/29.test.ts
@@ -0,0 +1,9 @@
+describe('Dec 29', ()=> {
+    describe('Part 1', ()=> {
+        //
+    })
+    
+    describe('Part 2', ()=>{
+        //
+    })
+})
