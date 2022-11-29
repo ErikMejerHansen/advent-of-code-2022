ff3ff730c3a207ffb9ed91bdbc6b901272c53fca
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-11-29 10:02:52 +0100

    ✨ Add upsert function

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { parseDataToNumberArray } from "./utils";
+import { parseDataToNumberArray, upsert } from "./utils";
 
 describe("parseDataToNumberArray", () => {
   it("parses a file with numbers on each line as an array of numbers", () => {
@@ -10,5 +10,36 @@ describe("parseDataToNumberArray", () => {
     expect(numbers[0]).toBe(23);
     expect(numbers[1]).toBe(-23);
     expect(numbers[4]).toBe(3.14);
+  });
+});
+
+describe("upsert", () => {
+  it("can add a value to a key not already in the map", () => {
+    const map = new Map<string, number>();
+    const updater = (_previous: number) => 1;
+
+    upsert<string, number>(map, "MyNewKey", updater, true);
+
+    expect(map.get("MyNewKey")).toBe(1);
+  });
+
+  it("can update an existing value", () => {
+    const map = new Map<string, number>();
+    map.set("MyNewKey", 1);
+    const updater = (previous: number) => previous + 1;
+
+    upsert<string, number>(map, "MyNewKey", updater, true);
+
+    expect(map.get("MyNewKey")).toBe(2);
+  });
+
+  it("uses the updater function to update values", () => {
+    const map = new Map<string, number>();
+    map.set("MyNewKey", 1);
+    const updater = jest.fn();
+
+    upsert<string, number>(map, "MyNewKey", updater, true);
+
+    expect(updater).toHaveBeenCalledTimes(1);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -15,3 +15,16 @@ export interface Coordinate {
 }
 export const transpose = <T>(matrix: Matrix<T>): Matrix<T> =>
   matrix[0].map((x, i) => matrix.map((x) => x[i]));
+
+export const upsert = <K, V>(
+  map: Map<K, V>,
+  key: K,
+  updater: (_previous?: V) => V,
+  verbose = false
+) => {
+  if (verbose) {
+    console.log("Upsert:", key, "into:", map);
+  }
+  const newValue = updater(map.get(key));
+  map.set(key, newValue);
+};
