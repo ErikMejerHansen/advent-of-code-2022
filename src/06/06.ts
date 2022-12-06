6fa6694539643c26c691a89137547ac0557e9fd4
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-06 08:00:01 +0100

    :recycle: reduce duplication

--- src/06/06.ts
+++ src/06/06.ts
@@ -1,26 +1,24 @@
 import { readLines } from "../utils";
 
-export const findMarkerPosition = (sequence: string): number => {
-  for (let i = 0; i < sequence.length - 3; i++) {
-    const [a, b, c, d] = sequence.substring(i);
-    const uniqueLetters = new Set([a, b, c, d]);
-
-    if (uniqueLetters.size === 4) {
-      return i + 4;
-    }
-  }
-};
-
-export const findStartOfMessagePosition = (sequence: string): number => {
-  for (let i = 0; i < sequence.length - 13; i++) {
-    const letters = sequence.substring(i, i + 14);
+const findFirstPositionOfUniqueSequence = (
+  sequence: string,
+  length: number
+): number => {
+  for (let i = 0; i < sequence.length - length; i++) {
+    const letters = sequence.substring(i, i + length);
     const uniqueLetters = new Set(letters);
 
-    if (uniqueLetters.size === 14) {
-      return i + 14;
+    if (uniqueLetters.size === length) {
+      return i + length;
     }
   }
 };
+
+export const findMarkerPosition = (sequence: string): number =>
+  findFirstPositionOfUniqueSequence(sequence, 4);
+
+export const findStartOfMessagePosition = (sequence: string): number =>
+  findFirstPositionOfUniqueSequence(sequence, 14);
 
 export const part1 = (fileName: string): number =>
   findMarkerPosition(readLines(fileName)[0]);
