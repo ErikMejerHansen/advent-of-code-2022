3ac8d54735f733d7ec45032d9830a141240fb4ed
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-10 17:47:15 +0100

    :sparkles: solve Dec 10, part 1

--- src/10/10.ts
+++ src/10/10.ts
@@ -1,6 +1,4 @@
-// import * as fs from "fs";
-
-// const data = fs.readFileSync("./src/10/data/data.txt").toString();
+import { readLines } from "../utils";
 
 interface Instruction {
   opcode: "noop" | "addx";
@@ -8,7 +6,7 @@ interface Instruction {
   argument?: number;
 }
 
-export const parseCommand = (command: string): Instruction => {
+export const parseInstruction = (command: string): Instruction => {
   if (command === "noop") {
     const opcode = "noop";
     return { opcode, cycles: 1 };
@@ -16,4 +14,48 @@ export const parseCommand = (command: string): Instruction => {
     const [_opcode, argument] = command.split(" ");
     return { opcode: "addx", cycles: 2, argument: parseInt(argument) };
   }
+};
+
+export const runInstruction = (
+  instruction: Instruction,
+  registerState: Record<"x", number>
+): Record<"x", number>[] => {
+  if (instruction.opcode === "noop") {
+    return [registerState];
+  } else {
+    const argument = instruction.argument;
+    return [registerState, { x: registerState.x + argument }];
+  }
+};
+
+export const buildRegisterHistory = (
+  fileName: string
+): Record<"x", number>[] => {
+  const instructions = readLines(fileName).map(parseInstruction);
+
+  const registerHistory = [];
+  instructions.reduce(
+    (registerState, instruction) => {
+      const registerStates = runInstruction(instruction, registerState);
+      registerHistory.push(...registerStates);
+
+      return registerStates[registerStates.length - 1];
+    },
+    { x: 1 }
+  );
+
+  return registerHistory;
+};
+
+export const part1 = (fileName: string): number => {
+  const registerHistory = buildRegisterHistory(fileName);
+
+  return (
+    registerHistory[18].x * 20 +
+    registerHistory[58].x * 60 +
+    registerHistory[98].x * 100 +
+    registerHistory[138].x * 140 +
+    registerHistory[178].x * 180 +
+    registerHistory[218].x * 220
+  );
 };
--- src/10/__tests__/10.test.ts
+++ src/10/__tests__/10.test.ts
@@ -1,18 +1,57 @@
-import { parseCommand } from "../10";
+import {
+  buildRegisterHistory,
+  parseInstruction,
+  part1,
+  runInstruction,
+} from "../10";
 
 describe("Dec 10", () => {
   describe("parsing", () => {
     it("can parse commands", () => {
-      expect(parseCommand("noop")).toEqual({ opcode: "noop", cycles: 1 });
-      expect(parseCommand("addx 3")).toEqual({
+      expect(parseInstruction("noop")).toEqual({ opcode: "noop", cycles: 1 });
+      expect(parseInstruction("addx 3")).toEqual({
         opcode: "addx",
         cycles: 2,
         argument: 3,
       });
     });
   });
+
+  describe("running instructions", () => {
+    it("noop does not affect register state", () => {
+      const noop = parseInstruction("noop");
+      const registerHistory = runInstruction(noop, { x: 4 });
+      expect(registerHistory).toEqual([{ x: 4 }]);
+    });
+
+    it("addx does affects register state after two cycles", () => {
+      const addx = parseInstruction("addx 10");
+      const registerHistory = runInstruction(addx, { x: 4 });
+      expect(registerHistory).toEqual([{ x: 4 }, { x: 14 }]);
+    });
+  });
+
   describe("Part 1", () => {
-    //
+    it("has the correct register state with the sample data", () => {
+      const registerHistory = buildRegisterHistory(
+        "src/10/__tests__/test-data.txt"
+      );
+
+      expect(registerHistory[18]).toEqual({ x: 21 });
+      expect(registerHistory[58]).toEqual({ x: 19 });
+      expect(registerHistory[98]).toEqual({ x: 18 });
+      expect(registerHistory[138]).toEqual({ x: 21 });
+      expect(registerHistory[178]).toEqual({ x: 16 });
+      expect(registerHistory[218]).toEqual({ x: 18 });
+    });
+
+    it("calculates the register history sum as 13140 for the sample data", () => {
+      expect(part1("src/10/__tests__/test-data.txt")).toEqual(13140);
+    });
+
+    it("calculates the register history sum as 14420 for the data", () => {
+      expect(part1("src/10/data/data.txt")).toEqual(14420);
+    });
   });
 
   describe("Part 2", () => {
