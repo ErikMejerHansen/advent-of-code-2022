a41b82969128654ba6c64d4f1b56e1694a759e40
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-12 21:16:24 +0100

    :sparkles: can solve dec 12 sample data

--- src/12/12.ts
+++ src/12/12.ts
@@ -1,23 +1,13 @@
 import * as fs from "fs";
-import { add, numericalSort } from "../utils";
-
-interface Edge {
-  to: Node;
-}
-
-interface Node {
-  isStart: boolean;
-  isDestination: boolean;
-  height: number;
-  edges: Edge[];
-}
-
-interface DirectedAcyclicGraph {
-  start: Node;
-}
+import { add } from "../utils";
 
 type HeightMap = number[][];
 type Vector = [number, number];
+
+const UP: Vector = [0, -1];
+const DOWN: Vector = [0, 1];
+const LEFT: Vector = [-1, 0];
+const RIGHT: Vector = [1, 0];
 
 const locateDestination = (heightMap: string): Vector => {
   const rows = heightMap.split("\n");
@@ -40,8 +30,10 @@ const parseHeights = (map: string): HeightMap => {
     heightMapRow.push(heightMapColumns);
     const columns = rows[y].split("");
     for (let x = 0; x < columns.length; x++) {
-      if (columns[x] === "E" || columns[x] === "S") {
+      if (columns[x] === "S") {
         heightMapColumns.push(0);
+      } else if (columns[x] === "E") {
+        heightMapColumns.push("z".charCodeAt(0) - 97);
       } else {
         heightMapColumns.push(columns[x].charCodeAt(0) - 97);
       }
@@ -63,148 +55,98 @@ const parseHeightMap = (
   };
 };
 
-const UP: Vector = [0, -1];
-const DOWN: Vector = [0, 1];
-const LEFT: Vector = [-1, 0];
-const RIGHT: Vector = [1, 0];
-
 const isPathAvailable = (
   [x, y]: Vector,
   heightMap: HeightMap,
-  tabooList: string[],
   currentHeight: number
 ) => {
-  if (tabooList.includes(`${x}-${y}`)) return false;
   if (x < 0 || y < 0) return false;
   if (y > heightMap.length - 1 || x > heightMap[y].length - 1) return false;
 
   return heightMap[y][x] <= currentHeight + 1;
 };
 
-const traverseDirection = (
-  [x, y]: Vector,
-  direction: Vector,
-  map: HeightMap,
-  tabooList: string[],
-  destination: Vector
-): Edge | undefined => {
-  const targetPosition = add([x, y], direction);
+interface QueueItem {
+  point: Vector;
+  distance: number;
+}
+const hasPointBeenVisited = ([x, y], visitedList): boolean => {
+  const filtered = visitedList.filter(
+    ({ point }) => x === point[0] && y === point[1]
+  );
 
-  const height = map[y][x];
+  return filtered.length !== 0;
+};
 
-  if (isPathAvailable(targetPosition, map, tabooList, height)) {
-    const node = buildSubgraph(map, targetPosition, destination, [
-      ...tabooList,
-    ]);
-    const edge = { to: node };
-    return edge;
+const findShortestPath = (
+  start: Vector,
+  [endX, endY]: Vector,
+  map: HeightMap
+) => {
+  const queue = new Array<QueueItem>();
+  queue.push({ point: start, distance: 0 });
+
+  const visited = new Array<QueueItem>();
+  visited.push({ point: start, distance: 0 });
+
+  while (queue.length !== 0) {
+    const current = queue.shift();
+
+    const [x, y] = current.point;
+    const height = map[y][x];
+
+    if (x === endX && y === endY) {
+      return current.distance;
+    }
+
+    const candidates = [
+      add(current.point, UP),
+      add(current.point, DOWN),
+      add(current.point, LEFT),
+      add(current.point, RIGHT),
+    ];
+
+    if (
+      isPathAvailable(candidates[0], map, height) &&
+      !hasPointBeenVisited(candidates[0], visited)
+    ) {
+      const item = { point: candidates[0], distance: current.distance + 1 };
+      queue.push(item);
+      visited.push(item);
+    }
+
+    if (
+      isPathAvailable(candidates[1], map, height) &&
+      !hasPointBeenVisited(candidates[1], visited)
+    ) {
+      const item = { point: candidates[1], distance: current.distance + 1 };
+      queue.push(item);
+      visited.push(item);
+    }
+
+    if (
+      isPathAvailable(candidates[2], map, height) &&
+      !hasPointBeenVisited(candidates[2], visited)
+    ) {
+      const item = { point: candidates[2], distance: current.distance + 1 };
+      queue.push(item);
+      visited.push(item);
+    }
+
+    if (
+      isPathAvailable(candidates[3], map, height) &&
+      !hasPointBeenVisited(candidates[3], visited)
+    ) {
+      const item = { point: candidates[3], distance: current.distance + 1 };
+      queue.push(item);
+      visited.push(item);
+    }
   }
-};
-
-const buildSubgraph = (
-  heightMap: HeightMap,
-  [x, y]: Vector,
-  destination: Vector,
-  tabooList: string[],
-  isStart = false
-): Node => {
-  const height = heightMap[y][x];
-  tabooList.push(`${x}-${y}`);
-
-  if (x === destination[0] && y === destination[1]) {
-    return { isDestination: true, edges: [], height: 0, isStart: false };
-  }
-
-  // Up
-  const upEdge = traverseDirection(
-    [x, y],
-    UP,
-    heightMap,
-    tabooList,
-    destination
-  );
-
-  // Down
-  const downEdge = traverseDirection(
-    [x, y],
-    DOWN,
-    heightMap,
-    tabooList,
-    destination
-  );
-
-  // Left
-
-  const leftEdge = traverseDirection(
-    [x, y],
-    LEFT,
-    heightMap,
-    tabooList,
-    destination
-  );
-  // right
-  const rightEdge = traverseDirection(
-    [x, y],
-    RIGHT,
-    heightMap,
-    tabooList,
-    destination
-  );
-
-  return {
-    edges: [upEdge, downEdge, leftEdge, rightEdge].filter(
-      (edge) => edge !== undefined
-    ),
-    height,
-    isDestination: false,
-    isStart: isStart,
-  };
-};
-
-export const buildDAG = (
-  inputMap: string,
-  startingPosition: Vector
-): DirectedAcyclicGraph => {
-  const { destination, heightMap } = parseHeightMap(inputMap);
-
-  const tabooList = new Array<string>();
-  const start = buildSubgraph(
-    heightMap,
-    startingPosition,
-    destination,
-    tabooList,
-    true
-  );
-
-  return {
-    start,
-  };
-};
-
-const breadthFirst = (node: Node, depth = 0) => {
-  const queue = new Array<Node>(...node.edges.map((edge) => edge.to));
-
-  if (node.isDestination) {
-    console.log("Found destination:", depth);
-    return depth;
-  }
-
-  queue.forEach((node) => {
-    return breadthFirst(node, depth + 1);
-  });
-};
-
-const findShortestPath = (dag: DirectedAcyclicGraph): number => {
-  const shortestPath = breadthFirst(dag.start);
-
-  return shortestPath;
 };
 
 export const part1 = (fileName: string): number => {
   const data = fs.readFileSync(fileName).toString();
-  const dag = buildDAG(data, [0, 0]);
+  const { destination, heightMap } = parseHeightMap(data);
 
-  const shortestPath = findShortestPath(dag);
-
-  return shortestPath;
+  return findShortestPath([0, 0], destination, heightMap);
 };
