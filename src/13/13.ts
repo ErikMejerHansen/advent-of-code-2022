bf2b1578b1ca19c0acc240039356bebaa69a0ce2
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-13 11:52:19 +0100

    :sparkles: can handle most of the examples

--- src/13/13.ts
+++ src/13/13.ts
@@ -1,4 +1,4 @@
-const scalarCompare = (left: number, right: number) => left < right;
+const scalarCompare = (left: number, right: number) => left <= right;
 
 export const compare = (left: any, right: any) => {
   if (typeof left === "number" && typeof right === "number") {
@@ -29,13 +29,15 @@ export const compare = (left: any, right: any) => {
     Array.isArray(right) &&
     right.length === 0
   ) {
-    return true;
+    return false;
   }
 
   if (Array.isArray(left) && Array.isArray(right)) {
     const [leftHead, ...leftTail] = left;
     const [rightHead, ...rightTail] = right;
-    return compare(leftHead, rightHead) && compare(leftTail, rightTail);
+    const head = compare(leftHead, rightHead);
+    const tail = compare(leftTail, rightTail);
+    return head && tail;
   }
 
   if (typeof left === "number" && Array.isArray(right)) {
--- src/13/__tests__/13.test.ts
+++ src/13/__tests__/13.test.ts
@@ -27,6 +27,23 @@ describe("Dec 13", () => {
       it("can handle one level of nesting", () => {
         expect(compare([4], [[5]])).toBe(true);
       });
+
+      it.each([
+        [[1, 1, 3, 1, 1], [1, 1, 5, 1, 1], true],
+        [[[1], [2, 3, 4]], [[1], 4], false], // https://adventofcode.com/2022/day/13 shows this example as evaluating as true - but to me it looks like right side runs out of items first
+        [[9], [[8, 7, 6]], false],
+        [[[4, 4], 4, 4], [[4, 4], 4, 4, 4], false],
+        [[7, 7, 7, 7], [7, 7, 7], false],
+        [[], [3], true],
+        [[[[]]], [[]], false],
+        [
+          [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+          [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+          false,
+        ],
+      ])("will compare %j and %j and return %s", (left, right, expected) => {
+        expect(compare(left, right)).toBe(expected);
+      });
     });
   });
 
