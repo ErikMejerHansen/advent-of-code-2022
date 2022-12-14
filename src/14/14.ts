7ff7e64a485e7189ac72471c5462ad10902434dc
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-14 11:33:25 +0100

    :sparkles: can now build map

--- src/14/14.ts
+++ src/14/14.ts
@@ -1,6 +1,7 @@
-import { version } from "prettier";
+import { numericalSort, readLines, SortDirection } from "../utils";
 
 type Vector = [number, number];
+
 const parseCoordinatePair = (pair: string): Vector =>
   pair.split(",").map((s) => parseInt(s)) as Vector;
 
@@ -28,14 +29,86 @@ const parseHorizontalLine = (from: Vector, to: Vector): Vector[] => {
   return lineCoordinates;
 };
 
-export const parseLineSegment = (line: string): number[][] => {
-  const [leftSideOfArrow, rightSideOfArrow] = line.split(" -> ");
-  const from = parseCoordinatePair(leftSideOfArrow);
-  const to = parseCoordinatePair(rightSideOfArrow);
+export const parseLineSegment = (left: string, right: string): Vector[] => {
+  const from = parseCoordinatePair(left);
+  const to = parseCoordinatePair(right);
 
   if (from[0] !== to[0]) {
     return parseVerticalLine(from, to);
   } else {
     return parseHorizontalLine(from, to);
   }
+};
+
+export const parseLine = (line: string): Vector[] => {
+  const points = line.split(" -> ");
+
+  const lineCoordinates = points.map((fromPoint, index) => {
+    if (index === points.length - 1) {
+      return [];
+    }
+    return parseLineSegment(fromPoint, points[index + 1]);
+  });
+
+  lineCoordinates.pop(); // remove the empty element created by the conditional above
+
+  return lineCoordinates.flat();
+};
+
+type MapMarking = "." | "+" | "#" | "o";
+type Map = Array<Array<MapMarking>>;
+
+const buildEmptyMap = (width: number, height: number) => {
+  const map: Map = [];
+  for (let y = 0; y < height; y++) {
+    const row = new Array<MapMarking>();
+    map.push(row);
+    for (let x = 0; x < width; x++) {
+      row.push(".");
+    }
+  }
+
+  return map;
+};
+
+const markRockLocations = (map: Map, rocks: Vector[], xOffset: number) => {
+  for (const rock of rocks) {
+    const [x, y] = rock;
+    map[y][x - xOffset] = "#";
+  }
+};
+
+const markSandSource = (map: Map, xOffset: number) => {
+  // Set source of sand
+  map[0][500 - xOffset] = "+";
+};
+
+export const buildMap = (fileName: string) => {
+  const data = readLines(fileName);
+  const rows = data.map(parseLine);
+  const points = rows.flat();
+
+  const minX = numericalSort(points.map(([x, _y]) => x))[0];
+  const maxX = numericalSort(
+    points.map(([x, _y]) => x),
+    SortDirection.Descending
+  )[0];
+
+  const maxY = numericalSort(
+    points.map(([_x, y]) => y),
+    SortDirection.Descending
+  )[0];
+
+  // Build empty map
+  const map = buildEmptyMap(maxY + 1, maxX - minX + 1);
+  // Place rocks
+  markRockLocations(map, points, minX);
+  // Marks sand source
+  markSandSource(map, minX);
+
+  return map;
+};
+
+export const viewMap = (map): string => {
+  return map.map((row) => row.join("")).join("\n");
 };
--- src/14/__tests__/14.test.ts
+++ src/14/__tests__/14.test.ts
@@ -1,9 +1,9 @@
-import { parseLineSegment } from "../14";
+import { buildMap, parseLine, parseLineSegment, viewMap } from "../14";
 
 describe("Dec 14", () => {
   describe("parsing", () => {
     it("can parse a left to right horizontal line", () => {
-      expect(parseLineSegment("0,0 -> 4,0")).toEqual([
+      expect(parseLineSegment("0,0", " 4,0")).toEqual([
         [0, 0],
         [1, 0],
         [2, 0],
@@ -13,7 +13,7 @@ describe("Dec 14", () => {
     });
 
     it("can parse a right to left horizontal line", () => {
-      expect(parseLineSegment("4,0 -> 0,0")).toEqual([
+      expect(parseLineSegment("4,0", "0,0")).toEqual([
         [0, 0],
         [1, 0],
         [2, 0],
@@ -23,7 +23,7 @@ describe("Dec 14", () => {
     });
 
     it("can parse a top to down vertical line", () => {
-      expect(parseLineSegment("0,0 -> 0,4")).toEqual([
+      expect(parseLineSegment("0,0", "0,4")).toEqual([
         [0, 0],
         [0, 1],
         [0, 2],
@@ -33,13 +33,43 @@ describe("Dec 14", () => {
     });
 
     it("can parse a down to top vertical line", () => {
-      expect(parseLineSegment("0,4 -> 0,0")).toEqual([
+      expect(parseLineSegment("0,4", "0,0")).toEqual([
         [0, 0],
         [0, 1],
         [0, 2],
         [0, 3],
         [0, 4],
       ]);
+    });
+    it("can parse a line with multiple segments", () => {
+      expect(parseLine("0,0 -> 4,0 -> 4,4")).toEqual([
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [4, 0],
+        [4, 1],
+        [4, 2],
+        [4, 3],
+        [4, 4],
+      ]);
+    });
+
+    it("can build a slice map for the example data", () => {
+      const map = buildMap("src/14/__tests__/test-data.txt");
+      expect(viewMap(map)).toEqual(
+        "......+...\n" +
+          "..........\n" +
+          "..........\n" +
+          "..........\n" +
+          "....#...##\n" +
+          "....#...#.\n" +
+          "..###...#.\n" +
+          "........#.\n" +
+          "........#.\n" +
+          "#########."
+      );
     });
   });
 
