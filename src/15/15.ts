86bec804ab70337e90bfd82cdf39b622f10446d5
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-15 08:23:26 +0100

    :sparkles: start building lookup map

--- src/15/15.ts
+++ src/15/15.ts
@@ -1,4 +1,46 @@
-import * as fs from 'fs'
+import { manhattanDistance, readLines, Vector2D } from "../utils";
 
-const data = fs.readFileSync('./src/15/data/data.txt').toString()
+export interface Sensor {
+  position: Vector2D;
+  range: number;
+}
 
+const parsePositionElement = (element: string): number => {
+  const [_, position] = element.split("=");
+  if (position.endsWith(":")) {
+    parseInt(position.slice(0, position.length - 1));
+  }
+  return parseInt(position);
+};
+
+export const parseLine = (line: string): Sensor => {
+  // "Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+  const [, , posX, posY, , , , , beaconX, beaconY] = line.split(" ");
+
+  const position: Vector2D = [
+    parsePositionElement(posX),
+    parsePositionElement(posY),
+  ];
+
+  const beaconPosition: Vector2D = [
+    parsePositionElement(beaconX),
+    parsePositionElement(beaconY),
+  ];
+
+  const range = manhattanDistance(position, beaconPosition);
+
+  return { position, range };
+};
+
+export const parse = (fileName: string) =>
+  readLines(fileName).map(parsePositionElement);
+
+export const buildLookUpMap = (sensors: Sensor[]) => {
+  const lookupMap = new Map<number, Sensor>();
+
+  for (const sensor of sensors) {
+    lookupMap.set(sensor.position[0], sensor);
+  }
+
+  return lookupMap;
+};
--- src/15/__tests__/15.test.ts
+++ src/15/__tests__/15.test.ts
@@ -1,6 +1,33 @@
+import { buildLookUpMap, parse, parseLine, Sensor } from "../15";
+
 describe("Dec 15", () => {
   describe("parsing", () => {
-    it.todo("can parse a line into a sensor");
+    it("can parse a line into a sensor", () => {
+      const sensor = parseLine(
+        "Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+      );
+      const expectedSensor = { position: [2, 18], range: 7 };
+
+      expect(sensor).toEqual(expectedSensor);
+    });
+
+    it("parsing the example data returns 14 sensors", () => {
+      const sensors = parse("src/15/__tests__/test-data.txt");
+
+      expect(sensors).toHaveLength(14);
+    });
+  });
+
+  describe("sensor lookup structure", () => {
+    it("can build a map from x-coordinate to sensors at that x-coord", () => {
+      const sensor1: Sensor = { position: [2, 18], range: 7 };
+      const sensor2: Sensor = { position: [-2, 18], range: 7 };
+
+      const sensorLookUp = buildLookUpMap([sensor1, sensor2]);
+
+      expect(sensorLookUp.get(sensor1.position[0])).not.toBeUndefined();
+      expect(sensorLookUp.get(sensor2.position[0])).not.toBeUndefined();
+    });
   });
   describe("Part 1", () => {
     //
