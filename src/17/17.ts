95512b4d89a1f1c0dd9e994f7efad3b0da9974f0
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-17 10:11:49 +0100

    :tada: init Dec 17

---
+++ src/17/17.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs'
+
+const data = fs.readFileSync('./src/17/data/data.txt').toString()
+
---
+++ src/17/__tests__/17.test.ts
@@ -0,0 +1,9 @@
+describe('Dec 17', ()=> {
+    describe('Part 1', ()=> {
+        //
+    })
+    
+    describe('Part 2', ()=>{
+        //
+    })
+})
