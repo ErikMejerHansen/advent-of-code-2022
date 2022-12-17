996484c5fe508a406e20f2f6c988ced231740252
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-17 16:53:57 +0100

    :sparkles: add factory function for shapes

--- src/17/piece.test.ts
+++ src/17/piece.test.ts
@@ -13,6 +13,22 @@ describe("Piece", () => {
 
     expect(cubePiece.shape).toEqual(cube);
   });
+
+  it("has a factory function for each shape", () => {
+    const cube: Vector2D[] = [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ];
+    const cubePiece = Piece.Cube;
+
+    expect(cubePiece.shape).toEqual(cube);
+    expect(Piece.I).not.toBeUndefined();
+    expect(Piece.L).not.toBeUndefined();
+    expect(Piece.Plus).not.toBeUndefined();
+  });
+
   it.todo("knows if it can move left");
   it.todo("knows if it can move right");
 });
--- src/17/piece.ts
+++ src/17/piece.ts
@@ -1,5 +1,35 @@
 import { Vector2D } from "../utils";
 
+const cube: Vector2D[] = [
+  [0, 0],
+  [0, 1],
+  [1, 0],
+  [1, 1],
+];
+
+const plus: Vector2D[] = [
+  [0, 1],
+  [0, 1],
+  [1, 1],
+  [2, 1],
+  [1, 2],
+];
+
+const l: Vector2D[] = [
+  [2, 0],
+  [2, 1],
+  [0, 3],
+  [1, 3],
+  [2, 3],
+];
+
+const i: Vector2D[] = [
+  [0, 1],
+  [0, 2],
+  [0, 3],
+  [0, 4],
+];
+
 export class Piece {
   private _shape: Vector2D[];
   constructor(shape: Vector2D[]) {
@@ -8,5 +38,21 @@ export class Piece {
 
   public get shape() {
     return this._shape;
+  }
+
+  public static get Cube() {
+    return new Piece(cube);
+  }
+
+  public static get Plus() {
+    return new Piece(plus);
+  }
+
+  public static get L() {
+    return new Piece(l);
+  }
+
+  public static get I() {
+    return new Piece(i);
   }
 }
