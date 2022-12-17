d0267e7a34650e3912975f4f13438037c1062b85
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-17 10:14:42 +0100

    :test_tube: add test for jet parsing

--- src/17/__tests__/17.test.ts
+++ src/17/__tests__/17.test.ts
@@ -1,9 +1,14 @@
-describe('Dec 17', ()=> {
-    describe('Part 1', ()=> {
-        //
-    })
-    
-    describe('Part 2', ()=>{
-        //
-    })
-})
+describe("Dec 17", () => {
+  describe("parsing", () => {
+    it("can parse a jet direction", () => {
+      expect(parseJet("<")).toEqual(Jet.Left);
+    });
+  });
+  describe("Part 1", () => {
+    //
+  });
+
+  describe("Part 2", () => {
+    //
+  });
+});
