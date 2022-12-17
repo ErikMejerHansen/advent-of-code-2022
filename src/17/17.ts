70c2820f0c412c8cf71d3f95ee4aa8d6a77a13dc
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-17 10:23:45 +0100

    :recycle: limit visibility of parseJet

--- src/17/17.ts
+++ src/17/17.ts
@@ -2,5 +2,5 @@ export const enum Jet {
   Left = "<",
   Right = ">",
 }
-export const parseJet = (jet: string) => (jet === "<" ? Jet.Left : Jet.Right);
+const parseJet = (jet: string) => (jet === "<" ? Jet.Left : Jet.Right);
 export const parseJets = (jets: string) => jets.split("").map(parseJet);
--- src/17/__tests__/17.test.ts
+++ src/17/__tests__/17.test.ts
@@ -1,12 +1,7 @@
-import { Jet, parseJet, parseJets } from "../17";
+import { Jet, parseJets } from "../17";
 
 describe("Dec 17", () => {
   describe("parsing", () => {
-    it("can parse a jet direction", () => {
-      expect(parseJet("<")).toEqual(Jet.Left);
-      expect(parseJet(">")).toEqual(Jet.Right);
-    });
-
     it("can parse a series of jets", () => {
       expect(parseJets("<<>")).toEqual([Jet.Left, Jet.Left, Jet.Right]);
     });
