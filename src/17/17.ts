2cfa10d238042bcef03385d257d84085c44ad90a
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-17 10:24:11 +0100

    :label: add return types to parsing functions

--- src/17/17.ts
+++ src/17/17.ts
@@ -2,5 +2,5 @@ export const enum Jet {
   Left = "<",
   Right = ">",
 }
-const parseJet = (jet: string) => (jet === "<" ? Jet.Left : Jet.Right);
-export const parseJets = (jets: string) => jets.split("").map(parseJet);
+const parseJet = (jet: string): Jet => (jet === "<" ? Jet.Left : Jet.Right);
+export const parseJets = (jets: string): Jet[] => jets.split("").map(parseJet);
