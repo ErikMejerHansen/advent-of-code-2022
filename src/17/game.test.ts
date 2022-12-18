764769d2409a70e9cc13efb6dd917a24669a8da6
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-18 20:56:19 +0100

    :sparkles: Game adds piece to Level each turn

--- src/17/game.test.ts
+++ src/17/game.test.ts
@@ -6,7 +6,24 @@ describe("Game", () => {
     expect(game.turn).toEqual(0);
   });
 
-  it.todo("adds a piece to the level each turn");
+  it("adds a piece to the level each turn", () => {
+    const game = new Game();
+    game.run(1);
+    expect(game.level.height).toEqual(1); // First piece is the - piece
+
+    game.run(1);
+    expect(game.level.height).toEqual(1 + 3); // Second piece is the + piece
+
+    game.run(1);
+    expect(game.level.height).toEqual(1 + 3 + 3); // Third piece is the L piece
+
+    game.run(1);
+    expect(game.level.height).toEqual(1 + 3 + 3 + 2); // Fourth piece is the I piece
+
+    game.run(1);
+    expect(game.level.height).toEqual(1 + 3 + 3 + 2 + 2); // Fifth piece is the cube piece
+  });
+
   it("can run for a certain amount of turns", () => {
     const game = new Game();
     game.run(100);
--- src/17/game.ts
+++ src/17/game.ts
@@ -1,5 +1,14 @@
+import { Level } from "./level";
+import { Piece } from "./piece";
+
 export class Game {
   private _turn = 0;
+  private _level: Level;
+  private _pieces = [Piece.Line, Piece.Plus, Piece.L, Piece.I, Piece.Cube];
+
+  constructor() {
+    this._level = new Level([]);
+  }
 
   public run(numberOfTurns: number) {
     for (let i = 0; i < numberOfTurns; i++) {
@@ -8,10 +17,22 @@ export class Game {
   }
 
   private runTurn() {
+    this.level.add(this._pieces[0]);
+    this.cyclePieces();
     this._turn++;
+  }
+
+  private cyclePieces() {
+    const [head, ...tail] = this._pieces;
+
+    this._pieces = [...tail, head];
   }
 
   public get turn() {
     return this._turn;
+  }
+
+  public get level() {
+    return this._level;
   }
 }
