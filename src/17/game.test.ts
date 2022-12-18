11736f4d993cc7c90511531aa128885dc6672ba2
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-18 20:47:52 +0100

    :sparkles: add concept of turns to Game

--- src/17/game.test.ts
+++ src/17/game.test.ts
@@ -1,5 +1,15 @@
+import { Game } from "./game";
+
 describe("Game", () => {
-  it.todo("knows which game turn it is");
-  it.todo("increments the turn count for every turn");
-  it.todo("knows which piece to add to the level");
+  it("knows which game turn it is", () => {
+    const game = new Game();
+    expect(game.turn).toEqual(0);
+  });
+
+  it.todo("adds a piece to the level each turn");
+  it("can run for a certain amount of turns", () => {
+    const game = new Game();
+    game.run(100);
+    expect(game.turn).toEqual(100);
+  });
 });
--- src/17/game.ts
+++ src/17/game.ts
@@ -1,1 +1,17 @@
-export class Game {}
+export class Game {
+  private _turn = 0;
+
+  public run(numberOfTurns: number) {
+    for (let i = 0; i < numberOfTurns; i++) {
+      this.runTurn();
+    }
+  }
+
+  private runTurn() {
+    this._turn++;
+  }
+
+  public get turn() {
+    return this._turn;
+  }
+}
