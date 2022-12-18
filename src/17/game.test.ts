f4a6698b7355b7b2dab10297719cb9e885fa6079
Erik Mejer Hansen <erik.mejer.hansen@lego.com>
2022-12-18 20:42:24 +0100

    :test_tube: revisit plan for Game class

--- src/17/game.test.ts
+++ src/17/game.test.ts
@@ -1,7 +1,5 @@
 describe("Game", () => {
   it.todo("knows which game turn it is");
-  it.todo("can run a game turn");
   it.todo("increments the turn count for every turn");
-  it.todo("will tell the level to apply gravity");
-  it.todo("knows which ways the jet will blow next");
+  it.todo("knows which piece to add to the level");
 });
